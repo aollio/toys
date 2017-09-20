7abdc575db8b17727104d8598621ade2797bc48f
aollio <aollio@outlook.com>
2017-09-20 22:54:57 +0800

    refactor

--- taobao-girl-spider/taobao-girl-spider.py
+++ taobao-girl-spider/taobao-girl-spider.py
@@ -57,49 +57,73 @@ class TaoBaoGirlSpider:
                                           desired_capabilities=dcap)
 
         self.driver.get('https://www.taobao.com/markets/mm/mmku')
+        # wait rendered
         time.sleep(2)
-        self.pages = int(self.driver.find_element_by_xpath('//*[@id="fn_page"]/div/span[@class="skip-wrap"]/em').text)
-        logging.info('总页数:', self.pages)
 
-        self.current_page = 1
+        self.categories = self.driver.find_elements_by_class_name("listing_tab_li")
+        logging.info('分类数:', len(self.categories))
+        [logging.info('     ', category.text) for category in self.categories]
 
     def start(self):
         logging.info('start.')
         try:
-            self.crawl()
+            self.crawl_all()
         except Exception:
             logging.error('出现异常')
         finally:
             self.close()
         logging.info('done.')
 
-    def crawl(self):
-        driver = self.driver
+    def crawl_all(self):
 
-        for page_index in range(self.current_page, self.pages + 1):
+        for category in self.categories:
+            # 点击分类, 当前页面渲染后, 开始解析当前页
+            logging.info('开始爬取分类 %s' % category.text)
+            category.click()
+            time.sleep(2)
+
+            # 初始化当前分类信息
+            self.current_category_text = category.text
+            self.pages = int(
+                self.driver.find_element_by_xpath('//*[@id="fn_page"]/div/span[@class="skip-wrap"]/em').text)
+            logging.info('分类 %s 总页数 %s' % (self.current_category_text, self.pages))
+            self.current_page = 1
+
+            # 爬取当前
+            self.craw_current_page()
+
+    def craw_current_page(self):
+        driver = self.driver
+        current_page = self.current_page
+        for page_index in range(current_page, self.pages + 1):
+            self.current_page = page_index
+
             soup = BeautifulSoup(driver.page_source, 'lxml')
-            logging.info('     当前第%s页' % page_index)
             # 每张图片的item在dom中class=cons_li
             cons_li_list = soup.select('.cons_li')
             len_cons_list = len(cons_li_list)
-            logging.info('     当前页的个数:', len_cons_list)
+            logging.info('分类: %s, 第%s页, 页中图片个数: %s'
+                         % (self.current_category_text, page_index, len_cons_list))
 
             for cons_li in cons_li_list:
-                name = cons_li.select('.item_name p')[0].get_text()
-                logging.info('         name:', name)
-                img = cons_li.select('.item_img img')[0]
-                img_src = img.get('src')
-                if img_src is None:
-                    img_src = img.get('data-ks-lazyload')
-                logging.info('         img_src:', img_src)
-
-                filename = name + os.path.splitext(img_src)[1]
-                save_img(name=filename, url=img_src)
+                self.parse_item(cons_li)
 
             if page_index == self.pages:
                 break
-            logging.info('     开始跳转到第%s页' % (page_index + 1))
             self.jump(page_index + 1)
+
+    def parse_item(self, item):
+        cons_li = item
+        name = cons_li.select('.item_name p')[0].get_text()
+        img = cons_li.select('.item_img img')[0]
+        img_src = img.get('src')
+        if img_src is None:
+            img_src = img.get('data-ks-lazyload')
+
+        logging.info('分类: %s, 第%s页, 姓名: %s, 图片地址: %s'
+                     % (self.current_category_text, self.current_page, name, img_src))
+        filename = name + os.path.splitext(img_src)[1]
+        save_img(name=filename, url=img_src)
 
     def jump(self, page_index):
         driver = self.driver
